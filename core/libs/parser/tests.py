ba9fc7e2c8da0575a1f0f1e9fd78f9bb94d3147b
Евгений Колодин <kolodin50056@gmail.com>
2022-12-04 22:17:59 +0300

    a

---
+++ core/libs/parser/tests.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from core.libs.parser.parser import Parser
+from core.models import Level
+from core.libs.enums.events import Event
+from core.libs.level_wrapper.level_wrapper import LevelWrapper
+
+class ParserTestCase(TestCase):
+    def setUp(self):
+        """level_map is equel 00000
+                              00000
+                              00000
+                              00000"""
+        Level.objects.create(level_map='11111100011010111111', width=5, height=4,
+                             x_start=3, y_start=3, x_finish=1, y_finish=2, num=1)
+        self.parser = Parser(
+            data='move;right;left;move',
+            level=LevelWrapper(1)
+        )
+
+    def test_level_parser(self):
+        self.assertEqual(self.parser.get_event(3, 3, 'upper'), Event.MOVE)
+        self.assertEqual(self.parser.get_event(2, 3, 'upper'), Event.ROTATE_RIGHT)
+        self.assertEqual(self.parser.get_event(2, 3, 'right'), Event.ROTATE_LEFT)
+        self.assertEqual(self.parser.get_event(2, 3, 'upper'), Event.DEFAULT)
